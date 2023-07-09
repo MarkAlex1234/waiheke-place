4f6a5059330688b2648713486b8185d0f3493f9d
MarkAlex1234 <mark.alexander1314@gmail.com>
2023-07-09 16:45:57 +1200

    feat/item-page

--- src/server/api/routers/listings.ts
+++ src/server/api/routers/listings.ts
@@ -3,10 +3,21 @@ import { createTRPCRouter, protectedProcedure, publicProcedure } from "~/server/
 
 export const listingsRouter = createTRPCRouter({
 
+    // List
     list: publicProcedure.query(({ ctx }) => {
         return ctx.prisma.listing.findMany();
     }),
 
+    // Get
+    get: publicProcedure.input(z.object({ id: z.string() })).query(({ input, ctx }) => {
+        return ctx.prisma.listing.findUnique({
+            where: {
+                id: input.id
+            }
+        })
+    }),
+
+    // Create
     create: protectedProcedure.input(z.object({ name: z.string(), description: z.string(), price: z.number() })).mutation(async ({ input, ctx }) => {
         return await ctx.prisma.listing.create({
             data: {
