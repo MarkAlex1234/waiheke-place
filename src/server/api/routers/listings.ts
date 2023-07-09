75618eaf46a1524b6b1d4449f4a673b30834b9ba
MarkAlex1234 <mark.alexander1314@gmail.com>
2023-07-09 15:34:37 +1200

    feat/add-item-card-component

--- src/server/api/routers/listings.ts
+++ src/server/api/routers/listings.ts
@@ -1,7 +1,12 @@
 import { z } from "zod";
-import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
+import { createTRPCRouter, protectedProcedure, publicProcedure } from "~/server/api/trpc";
 
 export const listingsRouter = createTRPCRouter({
+
+    list: publicProcedure.query(({ ctx }) => {
+        return ctx.prisma.listing.findMany();
+    }),
+
     create: protectedProcedure.input(z.object({ name: z.string(), description: z.string(), price: z.number() })).mutation(async ({ input, ctx }) => {
         return await ctx.prisma.listing.create({
             data: {
